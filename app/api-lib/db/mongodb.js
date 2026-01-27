8ebd4cec4e8a5f8f116438660ab90da418a9f6ec
Aravind R <ramachandran.aravind@gmail.com>
2026-01-26 19:22:45 -0500

    Hardcoding a lot of things

--- app/api-lib/db/mongodb.js
+++ app/api-lib/db/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 
-const uri = "mongodb://localhost:27017/BUDGET-DB"
+const uri = "mongodb://dbadmin:secret@database:27017/BUDGET-DB?authSource=admin"
 const options = {
 //  useUnifiedTopology: true,
   useNewUrlParser: false,
